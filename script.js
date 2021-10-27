e1bb0710981472b86e0ce2d53c9ee8c9ac0e902c
mahirshah8 <mahirshah8@gmail.com>
2021-10-27 18:11:53 +0500

    Update script.js
    MAKE CHILD OF CHUTU

--- script.js
+++ script.js
@@ -10,67 +10,203 @@ async function main() {
     try {
         await client.connect();
 
-
-
+   /*      await createListing(client,  { name: "الخدمة"
+        
+        
+        
+        
+        })
+ */
 
         /* await createListing(client, {
-      
-
-        })
         
         await updateListingByName(client, { childrenIds:   ["6177c1a82522759c5161e0d4","6177c1a82522759c5161e0d5","6177c1a82522759c5161e0d6","6177c1a82522759c5161e0d7","6177c1a82522759c5161e0d8","6177c1a82522759c5161e0d9","6177c1a82522759c5161e0da","6177c1a82522759c5161e0db"] }); */
 
 
 
-        await addDataToDatabase(client,  
+        await createmultipulListing(client,  
             [
                 {
-                    parentId: "6177d9382c10f970c0a3bf56",
-                    data: " تعديل  سجل تجاري",
+                    parentId: "617930fd8086b17f3afdbdb1",
+                    data: "إصدار سجل تجاري رئيسي",
                     childrenIds: []
                 },
                 {
-                    parentId: "6177d9382c10f970c0a3bf56",
-                    data: "وصف الخدمة",
+                    parentId: "617930fd8086b17f3afdbdb1",
+                    data: "إصدار",
                     childrenIds: []
                 },
                 {
-                    parentId: "6177d9382c10f970c0a3bf56",
-                    data: " شروط ومتطلبات الخدمة ",
+                    parentId: "617930fd8086b17f3afdbdb1",
+                    data: " إصدار سجل تجاري فرعي ",
                     childrenIds: []
                 },
                 {
-                    parentId: "6177d9382c10f970c0a3bf56",
-                    data: "خطوات الحصول على الخدمة ",
+                    parentId: "617930fd8086b17f3afdbdb2",
+                    data:  "تجديد",
+                    childrenIds: []
+                },
+                
+                {
+                    parentId: "617930fd8086b17f3afdbdb2",
+                    data: "تجديد سجل تجاري رئيسي",
+                    childrenIds: []
+                },               
+                {
+                    parentId: "617930fd8086b17f3afdbdb2",
+                    data: "	تجديد سجل تجاري فرعي ",
+                    childrenIds: []
+                },
+                
+                {
+                    parentId: "617930fd8086b17f3afdbdb2",
+                    data: "ما هي خدمة تجديد السجل التجاري؟ ",
                     childrenIds: []
                 },
                 {
-                    parentId: "6177d9382c10f970c0a3bf56",
-                    data: "رسوم الخدمة ",
+                    parentId: "617930fd8086b17f3afdbdb2",
+                    data: " ما هي شروط خدمة إصدار السجل التجاري ؟",
+                    childrenIds: []
+                },
+                
+                {
+                    parentId: "617930fd8086b17f3afdbdb2",
+                    data: "ما هي خطوات الحصول على خدمة تجديد السجل التجاري؟",
+                    childrenIds: []
+                },
+                
+                {
+                    parentId: "617930fd8086b17f3afdbdb2",
+                    data: "كم رسوم تجديد السجل التجاري ؟",
+                    childrenIds: []
+                },
+                
+                {
+                    parentId: "617930fd8086b17f3afdbdb2",
+                    data: "ما هي رسوم تجديد السجل التجاري ؟",
+                    childrenIds: []
+                },
+                
+                {
+                    parentId: "617930fd8086b17f3afdbdb2",
+                    data: "كم رسوم تجديد السجل التجاري الرئيسي؟",
+                    childrenIds: []
+                },
+                
+                {
+                    parentId: "617930fd8086b17f3afdbdb2",
+                    data: "كم رسوم تجديد السجل التجاري الفرعي؟",
+                    childrenIds: []
+                },
+                
+                {
+                    parentId: "617930fd8086b17f3afdbdb2",
+                    data: "كم مدة تنفيذ خدمة تجديد السجل التجاري ؟",
+                    childrenIds: []
+                },
+                
+                {
+                    parentId: "617930fd8086b17f3afdbdb2",
+                    data: "ما هو مكان تنفيذ خدمة تجديد السجل التجاري ؟",
                     childrenIds: []
                 },
                 {
-                    parentId: "6177d9382c10f970c0a3bf56",
-                    data: "مدة تنفيذ الخدمة  ",
+                    parentId: "617930fd8086b17f3afdbdb3",
+                    data: " تعيين مدير",
                     childrenIds: []
                 },
                 {
-                    parentId: "6177d9382c10f970c0a3bf56",
-                    data: "مكان تنفيذ الخدمة ",
+                    parentId: "617930fd8086b17f3afdbdb3",
+                    data: "	تعديل رأس مال سجل تجاري",
+                    childrenIds: []
+                },
+                {
+                    parentId: "617930fd8086b17f3afdbdb3",
+                    data:     "	تعديل عنوان سجل تجاري",
+                    childrenIds: []
+                },
+                {
+                    parentId: "617930fd8086b17f3afdbdb3",
+                    data: " عديل نشاط سجل تجاري " ,
+                    childrenIds: []
+                },
+                {
+                    parentId: "617930fd8086b17f3afdbdb3",
+                    data: " تعديل سجل تجاري فرعي", 
+                    childrenIds: []
+                },
+                {
+                    parentId: "617930fd8086b17f3afdbdb3",
+                    data:   "تعديل سجل تجاري رئيسي", 
+                    childrenIds: []
+                },
+                {
+                    parentId: "617930fd8086b17f3afdbdb3",
+                    data:  " تعديل",
+                    childrenIds: []
+                },
+                {
+                    parentId: "617930fd8086b17f3afdbdb5",
+                    data: "شطب", 
+                    childrenIds: []
+                },
+                {
+                    parentId: "617930fd8086b17f3afdbdb5",
+                    data:  " شطب سجل تجاري فرعي  " ,
+                    childrenIds: []
+                },
+                {
+                    parentId: "617930fd8086b17f3afdbdb5",
+                    data: "شطب سجل تجاري رئيسي",
+                    childrenIds: []
+                },
+                {
+                    parentId: "61792d8f6b5ecae82530dc8a",
+                    data: "شطب سجل تجاري لشخص متوفي",
+                    childrenIds: []
+                },
+                {
+                    parentId: "617930fd8086b17f3afdbdb6",
+                    data: "ملكية السجل التجاري" ,
+                    childrenIds: []
+                },
+                {
+                    parentId: "617930fd8086b17f3afdbdb6",
+                    data: "نقل",
+                    childrenIds: []
+                },
+                {
+                    parentId: "61792d8f6b5ecae82530dc8a",
+                    data: "نقل ملكية سجل تجاري من شخص متوفي",
+                    childrenIds: []
+                },
+                {
+                    parentId: "617930fd8086b17f3afdbdb7",
+                    data: ["تحويل سجل تجاري", "	تحويل"],
+                    childrenIds: []
+                },
+                {
+                    parentId: "617930fd8086b17f3afdbdb8",
+                    data: "المؤسسات الخيرية",
+                    childrenIds: []
+                },
+                {
+                    parentId: "617930fd8086b17f3afdbdb9",
+                    data: "المؤسسات الوقفية",
                     childrenIds: []
                 },
 
 
-            ]);
+            ]); 
 
-        /*    await addDataToDatabase(client,{
+    /*     /*    await addDataToDatabase(client,{
                  childrenIds:[],
                  data:"السعودي"
    
         
              });
     */
-
+ 
 
 
     } catch (error) {
@@ -84,7 +220,7 @@ async function main() {
 }
 
 
-main().catch(console.error);
+main().catch(console.error);/* 
 
 async function createListing(client, newListing) {
     const result = await client.db("test_db").collection("test_data").insertOne(newListing);
@@ -95,7 +231,7 @@ async function createListing(client, newListing) {
 
 
 }
-
+ */
 
 async function createmultipulListing(client, newListings) {
 
